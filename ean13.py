49c4b0c6da01d3395e149dc46fae07bf75fa678c
moebius <35337869+ErmanoBuikis@users.noreply.github.com>
2021-03-29 16:14:46 +0200

    Add files via upload

---
+++ ean13.py
@@ -0,0 +1,36 @@
+# Ermano Buikis
+
+import barcodenumber
+
+
+###################################
+# START NUMBER
+
+ean_number = '8053831640150'  # <<<---- HERE
+
+
+###################################
+
+
+
+tot_ean_requested = 300     # produce tot ean codes in txt format
+name_file_output = 'ean13_numbers.txt'
+ean_produced = 0
+
+
+if len(ean_number) != 13:
+   print ('error : your ean number has not 13 digits' )
+
+else:
+    while ean_produced < tot_ean_requested :
+    	valid = barcodenumber.check_code('ean13',ean_number)
+    	if valid:
+    	    with open(name_file_output, '+a') as f: 
+    	    # every restart of this program delete the file .txt
+    	        f.write( ean_number+'\n')
+            
+    	        ean_number = str(1+int(ean_number))
+    	        ean_produced += 1
+
+    	else:
+    	    ean_number = str(1+int(ean_number))
