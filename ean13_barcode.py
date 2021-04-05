567e7290e9b312285630cc1cdbb620c52fd82067
moebius <ermano.buikis@gmail.com>
2021-04-05 19:56:54 +0200

    commit

--- ean13_barcode.py
+++ ean13_barcode.py
@@ -1,16 +1,19 @@
 
 import barcode
 from barcode.writer import ImageWriter
+
 EAN = barcode.get_barcode_class('ean13')
 
-start = 805383785270
-num_code_to_generate = 3
+start = 805383785270      # <- START NUMBER
+num_code_to_generate = 3  # <- HOW MANY EAN BARCODE DO YOU WANT?
+
+ean_barcode_directory = "EAN_barcode/"
+
+
 stop = start + num_code_to_generate
 
-f = open('ean.txt', 'a+')
 for i in range(start,stop):
     code = str(i)
     ean = EAN(code, writer=ImageWriter())
     print(str(ean))
-    f.write(str(ean)+'\n')
-    fullname = ean.save('ean13_barcode_'+code)
+    fullname = ean.save(ean_barcode_directory+'ean13_barcode_'+code)
